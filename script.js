5085e43cffd8658c625d58a45ef0b932ad7dd140
Prohor <prohor-yakovlev@mail.ru>
2022-10-27 14:10:35 +0300

    task 37

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
 // task 33
-// // Setup
 // const lastName = 'Lovelace'
-
-// // Only change code below this line
 // const secondToLastLetterOfLastName = lastName[lastName.length - 2] // Change this line
-
 // console.log(secondToLastLetterOfLastName)
 
 // task 34
-
 // const myNoun = 'dog'
 // const myAdjective = 'big'
 // const myVerb = 'ran'
 // const myAdverb = 'quickly'
 
-// // Only change code below this line
 // const wordBlanks =
 //   myNoun + ' ' + myNoun + ' ' + myVerb + ' ' + myAdjective + ' ' + myAdverb // Change this line
-// // Only change code above this line
-
 // console.log(wordBlanks)
 
 // task 35
-
 // Only change code below this line
 // const myArray = ['peanut butter', 25]
-
 // console.log(myArray)
 
 // task 36
-
 // Only change code below this line
-const myArray = [
-  ['Prohor', 25],
-  ['Frontend', 'junor'],
-]
+// const myArray = [
+//   ['Prohor', 25],
+//   ['Frontend', 'junor'],
+// ]
+// console.log(myArray)
 
-console.log(myArray)
+// task 36
+const myArray = [50, 60, 70]
+const myData = myArray[0]
+console.log(myData)
