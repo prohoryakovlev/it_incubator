120ca4143495ecd5eca3519e2b9b9e3d3584aadf
Prohor <prohor-yakovlev@mail.ru>
2022-10-27 13:39:49 +0300

    task34

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// Setup
-// Setup
-const lastName = 'Lovelace'
+const myNoun = 'dog'
+const myAdjective = 'big'
+const myVerb = 'ran'
+const myAdverb = 'quickly'
 
 // Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length - 2] // Change this line
+const wordBlanks =
+  myNoun + ' ' + myNoun + ' ' + myVerb + ' ' + myAdjective + ' ' + myAdverb // Change this line
+// Only change code above this line
 
-console.log(secondToLastLetterOfLastName)
+console.log(wordBlanks)
