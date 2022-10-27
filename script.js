99a6da726b80ac8fdc2915d25fd5b230dbe68de1
Prohor <prohor-yakovlev@mail.ru>
2022-10-27 13:19:50 +0300

    task33

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Setup
+// Setup
 const lastName = 'Lovelace'
 
 // Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length - 1] // Change this line
-console.log(lastLetterOfLastName)
+const secondToLastLetterOfLastName = lastName[lastName.length - 2] // Change this line
+
+console.log(secondToLastLetterOfLastName)
