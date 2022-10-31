6af922b2814a4011aeb606bcca4cf878e800b0e7
Prohor <prohor-yakovlev@mail.ru>
2022-10-31 18:46:33 +0300

    Convert number to reversed array of digits

--- script.js
+++ script.js
@@ -132,7 +132,12 @@
 // }
 
 // CODEWARS  (Sleigh Authentication)
-function Sleigh() {}
-Sleigh.prototype.authenticate = function (name, password) {
-  return name === 'Santa Claus' && password === 'Ho Ho Ho!'
+// function Sleigh() {}
+// Sleigh.prototype.authenticate = function (name, password) {
+//   return name === 'Santa Claus' && password === 'Ho Ho Ho!'
+// }
+
+// CODEWARS  (Convert number to reversed array of digits)
+function digitize(n) {
+  return n.toString().split('').reverse().map(Number)
 }
