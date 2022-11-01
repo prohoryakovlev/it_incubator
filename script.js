fb2347c973ce768303570a6897891c6a8987a157
Prohor <prohor-yakovlev@mail.ru>
2022-11-01 13:37:02 +0300

    Training JS #3: Basic data types--String1~

--- script.js
+++ script.js
@@ -143,6 +143,35 @@
 // }
 
 // CODEWARS  (Keep Hydrated!)
-function litres(time) {
-  return Math.floor(time * 0.5)
+// function litres(time) {
+//   return Math.floor(time * 0.5)
+// }
+
+// CODEWARS  (Training JS #3: Basic data types--String)
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return ?+?+?;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return ?+?+?;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return ?+?+?;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "";
 }
