80d8a5460620f698d7f30c522b23597a1a0818d5
Prohor <prohor-yakovlev@mail.ru>
2022-11-01 16:40:49 +0300

    Set Alarm

--- script.js
+++ script.js
@@ -170,26 +170,31 @@
 // const answer3 = () => 'yes'
 
 // CODEWARS  (Training JS #4: Basic data types--Array)
-function getLength(arr) {
-  //return length of arr
-  return arr.length
-}
-function getFirst(arr) {
-  //return the first element of arr
-  return arr[0]
-}
-function getLast(arr) {
-  //return the last element of arr
-  return arr[arr.length - 1]
-}
-function pushElement(arr) {
-  var el = 1
-  //push el to arr
-  arr.push(el)
-  return arr
-}
-function popElement(arr) {
-  //pop an element from arr
-  arr.pop(arr)
-  return arr
+// function getLength(arr) {
+//   //return length of arr
+//   return arr.length
+// }
+// function getFirst(arr) {
+//   //return the first element of arr
+//   return arr[0]
+// }
+// function getLast(arr) {
+//   //return the last element of arr
+//   return arr[arr.length - 1]
+// }
+// function pushElement(arr) {
+//   var el = 1
+//   //push el to arr
+//   arr.push(el)
+//   return arr
+// }
+// function popElement(arr) {
+//   //pop an element from arr
+//   arr.pop(arr)
+//   return arr
+// }
+
+// CODEWARS  ( Set Alarm)
+function setAlarm(employed, vacation) {
+  return employed && !vacation
 }
