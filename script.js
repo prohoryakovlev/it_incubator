c7eea92caee9f95ee0703cdca7755f26739bceea
Prohor <prohor-yakovlev@mail.ru>
2022-11-01 13:38:21 +0300

    Training JS #3: Basic data types--String1~

--- script.js
+++ script.js
@@ -148,30 +148,23 @@
 // }
 
 // CODEWARS  (Training JS #3: Basic data types--String)
-var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
-function Dad(){
-  //select some variable to combine "Dad"
-  return ?+?+?;
-}
-function Bee(){
-  //select some variable to combine "Bee"
-  return ?+?+?;
-}
-function banana(){
-  //select some variable to combine "banana"
-  return ?+?+?;
-}
+var a1 = 'A',
+  a2 = 'a',
+  b1 = 'B',
+  b2 = 'b',
+  c1 = 'C',
+  c2 = 'c',
+  d1 = 'D',
+  d2 = 'd',
+  e1 = 'E',
+  e2 = 'e',
+  n1 = 'N',
+  n2 = 'n'
 
-//answer some questions if you finished works above
-function answer1(){
-  //the answer should be "yes" or "no"
-  return "";
-}
-function answer2(){
-  //the answer should be "yes" or "no"
-  return "";
-}
-function answer3(){
-  //the answer should be "yes" or "no"
-  return "";
-}
+const Dad = () => d1 + a2 + d2
+const Bee = () => b1 + e2 + e2
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2
+
+const answer1 = () => 'no'
+const answer2 = () => 'no'
+const answer3 = () => 'yes'
