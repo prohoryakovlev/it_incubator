dc514c26bd689956f97956426a57139fe4bccdf2
Prohor <prohor-yakovlev@mail.ru>
2022-11-01 20:27:19 +0300

    Student's Final Grade

--- script.js
+++ script.js
@@ -195,6 +195,19 @@
 // }
 
 // CODEWARS  ( Set Alarm)
-function setAlarm(employed, vacation) {
-  return employed && !vacation
+// function setAlarm(employed, vacation) {
+//   return employed && !vacation
+// }
+
+// CODEWARS  ( Student's Final Grade)
+function finalGrade(e, p) {
+  if (e > 90 || p > 10) {
+    return 100
+  } else if (e > 75 && p >= 5) {
+    return 90
+  } else if (e > 50 && p >= 2) {
+    return 75
+  } else {
+    return 0
+  }
 }
