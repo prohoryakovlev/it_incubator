d46971590038f7dbc0dac20efbba43546414a57e
Prohor <prohor-yakovlev@mail.ru>
2022-11-01 15:49:50 +0300

    Training JS #4: Basic data types--Array~

--- script.js
+++ script.js
@@ -148,23 +148,48 @@
 // }
 
 // CODEWARS  (Training JS #3: Basic data types--String)
-var a1 = 'A',
-  a2 = 'a',
-  b1 = 'B',
-  b2 = 'b',
-  c1 = 'C',
-  c2 = 'c',
-  d1 = 'D',
-  d2 = 'd',
-  e1 = 'E',
-  e2 = 'e',
-  n1 = 'N',
-  n2 = 'n'
+// var a1 = 'A',
+//   a2 = 'a',
+//   b1 = 'B',
+//   b2 = 'b',
+//   c1 = 'C',
+//   c2 = 'c',
+//   d1 = 'D',
+//   d2 = 'd',
+//   e1 = 'E',
+//   e2 = 'e',
+//   n1 = 'N',
+//   n2 = 'n'
 
-const Dad = () => d1 + a2 + d2
-const Bee = () => b1 + e2 + e2
-const banana = () => b2 + a2 + n2 + a2 + n2 + a2
+// const Dad = () => d1 + a2 + d2
+// const Bee = () => b1 + e2 + e2
+// const banana = () => b2 + a2 + n2 + a2 + n2 + a2
 
-const answer1 = () => 'no'
-const answer2 = () => 'no'
-const answer3 = () => 'yes'
+// const answer1 = () => 'no'
+// const answer2 = () => 'no'
+// const answer3 = () => 'yes'
+
+// CODEWARS  (Training JS #4: Basic data types--Array)
+function getLength(arr) {
+  //return length of arr
+  return arr.length
+}
+function getFirst(arr) {
+  //return the first element of arr
+  return arr[0]
+}
+function getLast(arr) {
+  //return the last element of arr
+  return arr[2]
+}
+function pushElement(arr) {
+  var el = 1
+  //push el to arr
+  arr.push(el)
+  return arr
+}
+function popElement(arr) {
+  //pop an element from arr
+  arr.pop(arr)
+  return arr
+}
