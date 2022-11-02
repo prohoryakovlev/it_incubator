df684c93ea2784649e7bceeb9cb8161688e19837
Prohor <prohor-yakovlev@mail.ru>
2022-11-02 08:47:04 +0300

    Reversing Words in a String

--- script.js
+++ script.js
@@ -231,6 +231,9 @@
 // }
 
 // CODEWARS  ( Enumerable Magic #25 - Take the First N Elements )
-function take(arr, n) {
-  return arr.slice(0, n)
-}
+// function take(arr, n) {
+//   return arr.slice(0, n)
+// }
+
+// CODEWARS  ( Reversing Words in a String )
+const reverse = (string) => string.split(' ').reverse().join(' ')
