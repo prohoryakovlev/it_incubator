7907d5ba6dcc3651b0cc6a94a8d538bc356ff2d8
Prohor <prohor-yakovlev@mail.ru>
2022-11-02 18:10:38 +0300

    Find Multiples of a Number

--- script.js
+++ script.js
@@ -257,9 +257,18 @@
 // }
 
 // CODEWARS  (Sum Arrays)
-function sum(numbers) {
-  let sum = 0
-  for (let i = 0; i < numbers.length; i++) sum += numbers[i]
+// function sum(numbers) {
+//   let sum = 0
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i]
 
-  return sum
+//   return sum
+// }
+
+// CODEWARS  (Find Multiples of a Number)
+function findMultiples(integer, limit) {
+  const arr = []
+  for (let i = integer; i <= limit; i += integer) {
+    arr.push(i)
+  }
+  return arr
 }
