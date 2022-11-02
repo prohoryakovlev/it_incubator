2448f1a3e320007f8499a8f55634615ae399b459
Prohor <prohor-yakovlev@mail.ru>
2022-11-02 19:00:36 +0300

    Find out whether the shape is a cube

--- script.js
+++ script.js
@@ -286,13 +286,19 @@
 // }
 
 // CODEWARS  (Mumbling)
-function accum(s) {
-  let res = ''
-  for (let i = 0; i < s.length; i++) {
-    res +=
-      s[i].toUpperCase() +
-      s[i].toLowerCase().repeat(i) +
-      (i < s.length - 1 ? '-' : '')
-  }
-  return res
+// function accum(s) {
+//   let res = ''
+//   for (let i = 0; i < s.length; i++) {
+//     res +=
+//       s[i].toUpperCase() +
+//       s[i].toLowerCase().repeat(i) +
+//       (i < s.length - 1 ? '-' : '')
+//   }
+//   return res
+// }
+
+// CODEWARS  (Find out whether the shape is a cube)
+var cubeChecker = function (volume, side) {
+  if (volume <= 0 || side <= 0) return false
+  return Math.cbrt(volume) === side
 }
