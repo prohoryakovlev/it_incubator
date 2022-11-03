dda5430310df12064dc00adf5884206cd8ca29b0
Prohor <prohor-yakovlev@mail.ru>
2022-11-03 14:05:33 +0300

    Will there be enough space?

--- script.js
+++ script.js
@@ -329,11 +329,17 @@
 // }
 
 // CODEWARS  (Sum of differences in array)
-function sumOfDifferences(arr) {
-  const sorted = arr.sort((a, b) => b - a)
-  let sum = 0
-  for (let i = 1; i < sorted.length; i++) {
-    sum += sorted[i - 1] - sorted[i]
-  }
-  return sum
+// function sumOfDifferences(arr) {
+//   const sorted = arr.sort((a, b) => b - a)
+//   let sum = 0
+//   for (let i = 1; i < sorted.length; i++) {
+//     sum += sorted[i - 1] - sorted[i]
+//   }
+//   return sum
+// }
+
+// CODEWARS  (Will there be enough space?)
+function enough(cap, on, wait) {
+  const p = on + wait - cap
+  return p < 0 ? 0 : p
 }
